5c604237333ed7310c8a1fff26edb716f76c7bef
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2022-12-08 19:00:51 +0000

    Secure Webpages

--- src/main/java/io/gitlab/markcrowe/servlets/WebPages.java
+++ src/main/java/io/gitlab/markcrowe/servlets/WebPages.java
@@ -2,10 +2,10 @@ package io.gitlab.markcrowe.servlets;
 
 public final class WebPages
 {
-	static final String ACCOUNT_DETAILS_JSP = "/account-details.jsp";
-	static final String HOME_JSP = "/home.jsp";
-	static final String SIGN_IN_JSP = "/sign-in.jsp";
-	static final String TEST_SECURITY_JSP = "/test-security.jsp";
+	static final String ACCOUNT_DETAILS_JSP = "/WEB-INF/account-details.jsp";
+	static final String HOME_JSP = "/WEB-INF/home.jsp";
+	static final String SIGN_IN_JSP = "/WEB-INF/sign-in.jsp";
+	static final String TEST_SECURITY_JSP = "/WEB-INF/test-security.jsp";
 
 	/**
 	 * Private constructor to prevent instantiation
