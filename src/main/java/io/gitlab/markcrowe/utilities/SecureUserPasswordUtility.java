61aa4d5f5959ce70cc3f577a9a1bd56d6e47d066
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2022-12-09 05:13:12 +0000

    Create SecureUserPasswordUtility.java

---
+++ src/main/java/io/gitlab/markcrowe/utilities/SecureUserPasswordUtility.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2022 Mark Crowe <https://github.com/markcrowe-com>. All rights reserved.
+ */
+package io.gitlab.markcrowe.utilities;
+
+import io.gitlab.markcrowe.User;
+import io.gitlab.markcrowe.repositories.UserRepository;
+
+public final class SecureUserPasswordUtility
+{
+	public static final String PERSISTENCE_UNIT_NAME = "default";
+
+	public static void main(String[] args)
+	{
+		hashUserPasswords(new UserRepository(PERSISTENCE_UNIT_NAME));
+		hashAndSaltUserPasswords(new UserRepository(PERSISTENCE_UNIT_NAME));
+		confirmUserHashAndSaltPasswords(new UserRepository(PERSISTENCE_UNIT_NAME));
+	}
+
+	public static void hashUserPasswords(UserRepository userRepository)
+	{
+		for(User user : userRepository.getAllUsers())
+		{
+			String hash = HashUtility.hashToHex(user.getPlainTextPassword());
+
+			user.setHashedPassword(hash);
+
+			userRepository.updateUser(user);
+		}
+	}
+
+	public static void hashAndSaltUserPasswords(UserRepository userRepository)
+	{
+		for(User user : userRepository.getAllUsers())
+		{
+			String salt = HashUtility.generateSalt();
+			String hash = HashUtility.hashToHex(user.getPlainTextPassword(), salt);
+
+			user.setHashedPassword(hash);
+			user.setSalt(salt);
+
+			userRepository.updateUser(user);
+		}
+	}
+
+	public static void confirmUserHashAndSaltPasswords(UserRepository userRepository)
+	{
+		for(User user : userRepository.getAllUsers())
+		{
+			String salt = user.getSalt();
+			String hash = HashUtility.hashToHex(user.getPlainTextPassword(), salt);
+
+			if(!user.getHashedPassword().equals(hash))
+				System.err.println(user.getUsername() + " hash is not valid");
+			else
+				System.out.println(user.getUsername() + " hash is valid");
+		}
+	}
+
+	/**
+	 * Private constructor to prevent instantiation
+	 */
+	private SecureUserPasswordUtility()
+	{
+		throw new IllegalStateException("Utility class");
+	}
+}
